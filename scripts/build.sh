2b1820889a411a9f85fd84f45ad87b7b9979f5ed
godcheese <godcheese@outlook.com>
2024-03-19 21:24:39 +0800

    feat: 增加构建工作流

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,4 @@ rustup target add x86_64-unknown-linux-musl
 cargo build --release --target=x86_64-unknown-linux-musl
 rustup target add x86_64-pc-windows-gnu
 cargo build --release --target=x86_64-pc-windows-gnu
-
 cd $current_path
